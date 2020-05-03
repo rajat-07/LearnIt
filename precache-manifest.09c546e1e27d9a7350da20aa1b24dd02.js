7b63a728bf24129c6294cd3930ebbb5650c96bdc
rajat-07 <rajatparab07@gmail.com>
2020-05-03 21:40:56 +0530

    Updates

---
+++ precache-manifest.09c546e1e27d9a7350da20aa1b24dd02.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7ec72ee5779a15b3d340cc3f708520",
+    "url": "/LearnIt/index.html"
+  },
+  {
+    "revision": "b855285eecca12f1a71b",
+    "url": "/LearnIt/static/css/2.d623fad8.chunk.css"
+  },
+  {
+    "revision": "e11880b82336aaa0d3d8",
+    "url": "/LearnIt/static/css/main.a3dd70c7.chunk.css"
+  },
+  {
+    "revision": "b855285eecca12f1a71b",
+    "url": "/LearnIt/static/js/2.5af9846a.chunk.js"
+  },
+  {
+    "revision": "00ba9122ace4f7cfb2b78b0b307611b4",
+    "url": "/LearnIt/static/js/2.5af9846a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e11880b82336aaa0d3d8",
+    "url": "/LearnIt/static/js/main.57362a68.chunk.js"
+  },
+  {
+    "revision": "6f9da56ea3385e3e4461",
+    "url": "/LearnIt/static/js/runtime-main.0bc98a64.js"
+  },
+  {
+    "revision": "81d9dc66666175fa17fa7116e2b1baa2",
+    "url": "/LearnIt/static/media/banner-2.81d9dc66.jpg"
+  },
+  {
+    "revision": "4edf95e538de20a87c34caef795f77d5",
+    "url": "/LearnIt/static/media/banner.4edf95e5.jpg"
+  },
+  {
+    "revision": "433d63a0498ae795691fe48ecba9f08c",
+    "url": "/LearnIt/static/media/course-1.433d63a0.jpg"
+  },
+  {
+    "revision": "7304c87299b0d5253ab02023969d167f",
+    "url": "/LearnIt/static/media/course-2.7304c872.jpg"
+  },
+  {
+    "revision": "9508293795a14f851c9b1c598b766f57",
+    "url": "/LearnIt/static/media/course-3.95082937.jpg"
+  },
+  {
+    "revision": "1bb2b14bf17b9cc409a283facb2f2c7b",
+    "url": "/LearnIt/static/media/course-4.1bb2b14b.jpg"
+  },
+  {
+    "revision": "7cfea53811e8054da0295bcf7eb21410",
+    "url": "/LearnIt/static/media/course-5.7cfea538.jpg"
+  },
+  {
+    "revision": "5b7db7d0b1c2cfd19ae1f3b7b1d77854",
+    "url": "/LearnIt/static/media/home-banner.5b7db7d0.jpg"
+  },
+  {
+    "revision": "04b2f92abfec98d9e2bf8e90401610a1",
+    "url": "/LearnIt/static/media/l-blog-1.04b2f92a.jpg"
+  },
+  {
+    "revision": "cb1dfa109f8cd2aa6f357121feb9f077",
+    "url": "/LearnIt/static/media/l-blog-2.cb1dfa10.jpg"
+  },
+  {
+    "revision": "6886c9529b2e7038002ecdc20f79e5ae",
+    "url": "/LearnIt/static/media/l-blog-3.6886c952.jpg"
+  },
+  {
+    "revision": "122d5f4d5936bed9c465f40414cb092c",
+    "url": "/LearnIt/static/media/l-blog-4.122d5f4d.jpg"
+  },
+  {
+    "revision": "89b85f8d7a5f10591bd991cd55301075",
+    "url": "/LearnIt/static/media/notify-bg.89b85f8d.jpg"
+  },
+  {
+    "revision": "4feff250e7e2485824b8337b9b2921e4",
+    "url": "/LearnIt/static/media/team-1.4feff250.jpg"
+  },
+  {
+    "revision": "965dad5bb53d60993893429f81beb57d",
+    "url": "/LearnIt/static/media/team-2.965dad5b.jpg"
+  },
+  {
+    "revision": "57e62c6cdf60ea7b6bc649d1ab6c05ae",
+    "url": "/LearnIt/static/media/team-3.57e62c6c.jpg"
+  },
+  {
+    "revision": "a0f5a10502087c1d1026f0bb4a66eae8",
+    "url": "/LearnIt/static/media/team-4.a0f5a105.jpg"
+  },
+  {
+    "revision": "4603d0e22b5a227289bdd5688291739e",
+    "url": "/LearnIt/static/media/testi-3.4603d0e2.png"
+  },
+  {
+    "revision": "601968cb24712b60bbfd3c1ac2b2275b",
+    "url": "/LearnIt/static/media/video-1.601968cb.jpg"
+  }
+]);
